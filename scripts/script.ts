fddad1a9a1660aebc17b47ad927473f9f51bf1dd
Denis Kramarenko <cashflow.dkm@gmail.com>
2025-03-10 22:08:31 +1000

    First Commit

---
+++ scripts/script.ts
@@ -0,0 +1,3 @@
+fetch(
+  'https://api.openweathermap.org/data/2.5/weather?lat=43.13&lon=131.91&units=metric&appid=09fb599dd2980efff0d48342ac50f502'
+).then(res => res.json()).then(data => console.log(data.main.temp));
