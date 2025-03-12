a48deec021285bfbc49ee0cb991332a9febbe486
Denis Kramarenko <cashflow.dkm@gmail.com>
2025-03-12 14:11:47 +1000

    Complete Edition

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=43.13&lon=131.91&units=metric&appid=09fb599dd2980efff0d48342ac50f502').then(res => res.json()).then(data => console.log(data.main.temp));
+'use strict';
+const degrees = document.getElementsByClassName('degrees');
+const weatherInfo = document.getElementsByClassName('weather__info__mode');
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=43.30&lon=5.37&units=metric&appid=09fb599dd2980efff0d48342ac50f502')
+    .then((res) => res.json())
+    .then((data) => {
+    degrees[0].textContent = data.main.temp;
+    weatherInfo[0].textContent = data.weather[0].main;
+});
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=41.89&lon=12.48&units=metric&appid=09fb599dd2980efff0d48342ac50f502')
+    .then((res) => res.json())
+    .then((data) => {
+    degrees[1].textContent = data.main.temp;
+    weatherInfo[1].textContent = data.weather[0].main;
+});
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=42.33&lon=10.31&units=metric&appid=09fb599dd2980efff0d48342ac50f502')
+    .then((res) => res.json())
+    .then((data) => {
+    degrees[2].textContent = data.main.temp;
+    weatherInfo[2].textContent = data.weather[0].main;
+});
